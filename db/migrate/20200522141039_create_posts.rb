6f00f828adbdc3ca07adf5c19bb8c15ce01e93e2
MuAladdinIbrahim <muhammadalaa27@gmail.com>
2020-05-22 16:15:26 +0200

    post scaffold

---
+++ db/migrate/20200522141039_create_posts.rb
@@ -0,0 +1,9 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.references :user
+      t.references :calculation
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_134519) do
+ActiveRecord::Schema.define(version: 2020_05_22_141039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2020_05_22_134519) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["system_id"], name: "index_calculations_on_system_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "calculation_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["calculation_id"], name: "index_posts_on_calculation_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "systems", force: :cascade do |t|
