6047d53519797041c21809407432f863ead1b2fb
MuAladdinIbrahim <muhammadalaa27@gmail.com>
2020-05-29 04:20:34 +0200

    post validation-crud

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_123941) do
+ActiveRecord::Schema.define(version: 2020_05_28_124556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 2020_05_27_123941) do
     t.bigint "calculation_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title", null: false
+    t.text "description", null: false
     t.index ["calculation_id"], name: "index_posts_on_calculation_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
