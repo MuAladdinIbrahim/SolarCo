e1df01f53022cba1be1873d53cdfdbbe172c3eed
Nouran Samy <nourans.96@yahoo.com>
2020-05-31 14:30:11 +0200

    Abilities for contractor

--- app/models/contractor.rb
+++ app/models/contractor.rb
@@ -7,4 +7,19 @@ class Contractor < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  has_one_attached :avatar
+
+  # Returns the url path for the avatar blob
+  def avatar_url
+      self.avatar.attachment.nil? ? '' :
+      "#{Rails.configuration.api_url}#{Rails.application.routes.url_helpers.rails_blob_path(self.avatar, only_path: true)}"
+  end
+
+  def as_json(options={})
+    super(options).merge({
+        rules: Ability.new(self).as_json,
+        avatar: self.avatar_url
+    })
+  end
 end
