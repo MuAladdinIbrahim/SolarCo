1da47f56f419c3dbc97c68f8cd4fed51121354a4
Ahmed-Abd-elhamid <eng.a.abdelhamid@outlook.com>
2020-06-05 15:16:59 +0200

    check on geocoder and user_ip

--- app/controllers/geocoder_controller.rb
+++ app/controllers/geocoder_controller.rb
@@ -7,25 +7,27 @@ class GeocoderController < ApiController
     if geocoder_params[:lat] && geocoder_params[:long] && res_loc = (Geocoder.search([geocoder_params[:lat].round(6), geocoder_params[:long].round(6)])[0].data).to_hash['address']
 
       puts (Geocoder.search([geocoder_params[:lat].round(6), geocoder_params[:long].round(6)])[0].data).to_yaml
-      
+
       render json: jsonFormat(geocoder_params[:lat].round(6), geocoder_params[:long].round(6), res_loc['city'], res_loc['country'])
     else
       getbyIP
     end
   end
 
-  # private
   
   def getbyIP
-    res_ip = (Geocoder.search(geocoder_params[:ip])[0].data).to_hash
+    ip = geocoder_params[:ip] || request.remote_ip
+    res_ip = (Geocoder.search(ip)[0].data).to_hash
     loc = res_ip['loc'].split(',') unless res_ip['loc'].nil?
-
+    
     render json: jsonFormat(loc[0].to_f.round(6), loc[1].to_f.round(6), res_ip['city'], res_ip['country']) 
   end
-
+  
   def jsonFormat(lat, long, city, country)
     {"consumption" => geocoder_params[:consump], "ip" => geocoder_params[:ip], "latitude" => lat, "longitude" => long, "city" => city, "country" => country, "permission" => true}
   end
+  
+  private
 
   def geocoder_params
     params.require(:geocoder).permit(:id, :src, :consump, :ip, :lat, :long)
