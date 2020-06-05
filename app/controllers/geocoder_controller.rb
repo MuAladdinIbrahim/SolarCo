857e246d0f9c1e7673e6920f74520b105c3ffa81
Ahmed-Abd-elhamid <eng.a.abdelhamid@outlook.com>
2020-06-05 16:36:43 +0200

    validate request on geocoder

--- app/controllers/geocoder_controller.rb
+++ app/controllers/geocoder_controller.rb
@@ -1,9 +1,6 @@
 class GeocoderController < ApiController
 
   def getLocation    
-    puts request.remote_ip
-    puts geocoder_params.to_yaml
-
     if geocoder_params[:lat] && geocoder_params[:long] && res_loc = (Geocoder.search([geocoder_params[:lat].round(6), geocoder_params[:long].round(6)])[0].data).to_hash['address']
 
       puts (Geocoder.search([geocoder_params[:lat].round(6), geocoder_params[:long].round(6)])[0].data).to_yaml
@@ -13,21 +10,24 @@ class GeocoderController < ApiController
       getbyIP
     end
   end
-
+  
+  private
   
   def getbyIP
     ip = geocoder_params[:ip] || request.remote_ip
-    res_ip = (Geocoder.search(ip)[0].data).to_hash
-    loc = res_ip['loc'].split(',') unless res_ip['loc'].nil?
-    
-    render json: jsonFormat(loc[0].to_f.round(6), loc[1].to_f.round(6), res_ip['city'], res_ip['country']) 
+    if ( ip )
+      res_ip = (Geocoder.search(ip)[0].data).to_hash
+      loc = res_ip['loc'].split(',') unless res_ip['loc'].nil?
+      
+      render json: jsonFormat(loc[0].to_f.round(6), loc[1].to_f.round(6), res_ip['city'], res_ip['country'])
+    else
+      render json: {"permission" => false}
+    end
   end
   
   def jsonFormat(lat, long, city, country)
     {"consumption" => geocoder_params[:consump], "ip" => geocoder_params[:ip], "latitude" => lat, "longitude" => long, "city" => city, "country" => country, "permission" => true}
-  end
-  
-  private
+  end 
 
   def geocoder_params
     params.require(:geocoder).permit(:id, :src, :consump, :ip, :lat, :long)
