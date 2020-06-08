4c5eb09516217d77e7485ae2859d5cb22802480f
zeyadsaleh <zeyadmostafasaleh@gmail.com>
2020-06-08 06:48:48 +0200

    adding count offers to post homepage and offers index

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -4,9 +4,10 @@ class OffersController < ApiController
 
   # GET /offers/post/1  -> end-point to retrieve all offer from specific post
   def getOffers
-    @offers = Offer.where(post_id: params[:post_id]).all
+    offers_per_post = Offer.where(post_id: params[:post_id])
+    @offers = offers_per_post.order(created_at: :asc).limit(10)
 
-    render json: @offers.as_json(include: [{contractor: { methods: [:avatar_url] }}, :post])
+    render json: @offers.as_json(include: [{contractor: { methods: [:avatar_url] }}, :post,])
   end
 
   # GET /offers
--- db/migrate/20200522141039_create_posts.rb
+++ db/migrate/20200522141039_create_posts.rb
@@ -2,6 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.boolean :closed, :default => false
+      t.integer :offers_count
       t.references :user, null: false, foreign_key: true
       t.references :post, null: false, foreign_key: true
       t.timestamps
