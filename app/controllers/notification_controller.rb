3fc4e022fa851d2ed7ee3cc54e1a131b5bcef0ec
Nouran Samy <nourans.96@yahoo.com>
2020-06-12 16:01:48 +0200

    Notifications for chat

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -2,19 +2,31 @@ class NotificationController < ApiController
 
   def index
     if current_user
+      puts current_user.id
       @user_posts = Post.where(user_id: current_user.id)
+
       @post_offers = Offer.where(post_id: @user_posts.ids)
-      @notifications = PublicActivity::Activity.order("created_at DESC").where(owner_type: "Contractor", trackable_type: "Offer", trackable_id: @post_offers.ids ).all
+
+      @notifications = PublicActivity::Activity.order("created_at DESC").where("(recipient_type = ? AND trackable_type = ? AND recipient_id = ?) OR (owner_type = ? AND trackable_type =? AND trackable_id IN (?))", "User", "Message", current_user.id, "Contractor", "Offer", @post_offers.ids.each {|id| id.to_s + ','}).all
+
     elsif current_contractor
       @contractor_offers_ = Offer.select(:post_id).where(contractor_id: current_contractor.id)
+
       @offers_posts = Post.where(id: @contractor_offers)
-      @notifications = PublicActivity::Activity.order("created_at DESC").where(owner_type: "User", trackable_type: "Post", trackable_id: @offers_posts.ids).all
+
+      @notifications = PublicActivity::Activity.order("created_at DESC").where("(recipient_type = ? AND trackable_type = ? AND recipient_id = ?) OR (owner_type = ? AND trackable_type = ? AND trackable_id IN (?))", "Contractor", "Message", current_contractor.id, "User", "Post", @offers_posts.ids.each {|id| id.to_s + ','}).all
+
+      # @chatNotifications = PublicActivity::Activity.order("created_at DESC").where(recipient_type: "Contractor", trackable_type: "Message", recipient_id: current_contractor.id ).all
+
+      # @notifications = PublicActivity::Activity.order("created_at DESC").where(owner_type: "User", trackable_type: "Post", trackable_id: @offers_posts.ids).all
+
+      # @notifications += @chatNotifications
     end
-    render json: @notifications.as_json(include: [:trackable,:owner])
+    render json: {data: @notifications.as_json(include: [:trackable,:owner])}
   end
 
   def show
-    @notofication = PublicActivity::Activity.find(params[:id])
+    @notification = PublicActivity::Activity.find(params[:id])
     render json: @notification
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,8 @@
 class Message < ApplicationRecord
+  include PublicActivity::Model
+  tracked owner: ->(controller, model) { model && model.messagable }
+  tracked recipient: ->(controller, model) { model && model.getRecipient }
+  
   belongs_to :chatroom
   belongs_to :messagable, polymorphic: true
 
@@ -10,5 +14,15 @@ class Message < ApplicationRecord
       super(options).merge({
           sender: self.messagable
       })
+  end
+
+  def getRecipient
+    @chatroom = Chatroom.find(chatroom_id)
+
+    if self.messagable_type == 'User'
+      Contractor.find(@chatroom.contractor_id)
+    elsif self.messagable_type == 'Contractor'
+      User.find(@chatroom.user_id)
+    end
   end
 end
